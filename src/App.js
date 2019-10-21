2cc708b670192fd90d135a6d0baafd6cb884ffa0
Elmi Rabelo <rabeloelmi@gmail.com>
2019-10-20 21:46:55 -0300

    Welcome page configurada, agora recebe nome do user e verifica na api do github se ele existe, guardando a informação no state e AsyncStorage e por fim enviando para a proxima apagina

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import {View, Text} from 'react-native';
 
 import Routes from './routes';
-
+console.tron.log('Olá');
 const App = () => <Routes />;
 
 export default App;
